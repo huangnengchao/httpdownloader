4a7dfbb935aa0f5b00705c5d6901f66fb2ff457e
nil <nilzero@outlook.com>
2018-10-19 15:12:12 +0800

    add zlib build bat

---
+++ 3rdparty/build-zlib-win-gcc-debug.bat
@@ -0,0 +1,21 @@
+set COMPILER=gcc
+set OSTYPE=win
+set BUILDTYPE=debug
+
+
+cd zlib-1.2.11
+set ZLIB_ROOT=%cd%
+
+set INSTALL_PATH=%ZLIB_ROOT%\..\..\lib\%OSTYPE%\%COMPILER%\%BUILDTYPE%\zlib
+
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%
+
+mingw32-make
+mingw32-make install
+
+::重命名lib
+cd %INSTALL_PATH%/lib
+
+ren libzlibstatic.a libz.a
+
+cd .
